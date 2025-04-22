51d1bf9c884fef25f8c6569df66fd632889d8e19
simonerao <sirao@ucsd.edu>
2025-04-21 18:23:39 -0700

    update

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,10 @@
-import { fetchJSON, renderProjects } from '../global.js';
-const projects = await fetchJSON('../lib/projects.json');
-const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
+import { fetchJSON, renderProjects } from './global.js';
+
+const data = await fetchJSON('./lib/projects.json');
+renderProjects(data, document.querySelector('.projects'), 'h3');
+
+// Count the projects and update the title
+const titleElement = document.querySelector('.projects-title');
+if (titleElement) {
+  titleElement.textContent = `Projects (${data.length})`;
+}
