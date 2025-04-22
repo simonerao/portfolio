6ae601946447f5d66f9bad676eba9062ef52da39
simonerao <sirao@ucsd.edu>
2025-04-21 19:14:07 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,12 +2,11 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json'); 
 const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
 
 const projectsTitle = document.querySelector('.projects-title');
-
-// Update the text content of the title to include the count of projects
 projectsTitle.textContent = `Projects (${projects.length})`;
+
+renderProjects(projects, projectsContainer, 'h2');
 
 console.log("Projects loaded:", projects);
 
