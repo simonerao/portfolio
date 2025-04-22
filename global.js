6607ee93d0d6bfa05606e3441ec3b45ce0a32ad2
simonerao <sirao@ucsd.edu>
2025-04-21 17:49:46 -0700

    Update global.js

--- global.js
+++ global.js
@@ -123,7 +123,10 @@ export async function fetchJSON(url) {
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
-    
+
+    const data = await response.json();
+    return data;
+
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
