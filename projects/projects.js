00a868dfb9699ca829b1204ed17ee8ca5099b92e
simonerao <sirao@ucsd.edu>
2025-04-29 16:34:04 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -31,14 +31,16 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // ======= NEW: Draw pie chart using D3 =======
 
-    const data = [
-      { value: 1, label: 'apples' },
-      { value: 2, label: 'oranges' },
-      { value: 3, label: 'mangos' },
-      { value: 4, label: 'pears' },
-      { value: 5, label: 'limes' },
-      { value: 5, label: 'cherries' }
-    ];
+    const rolledData = d3.rollups(
+      projects,
+      (v) => v.length,
+      (d) => d.year
+    );
+    
+    const data = rolledData.map(([year, count]) => {
+      return { value: count, label: year };
+    });
+    
 
     const arcGenerator = d3.arc()
       .innerRadius(0)
