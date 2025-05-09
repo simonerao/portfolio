dc884b1a7f3846b0da55c6e3f41c83bb8ab7f974
simonerao <sirao@ucsd.edu>
2025-05-09 15:30:57 -0700

    update

--- meta/main.js
+++ meta/main.js
@@ -79,16 +79,7 @@ function renderCommitInfo(data, commits) {
   const avgDepth = d3.mean(data, d => d.depth);
   dl.append('dt').text('Average depth');
   dl.append('dd').text(avgDepth.toFixed(2));
-
-  // Average file depth
-  const fileDepths = d3.rollups(data, v => d3.max(v, d => d.depth), d => d.file);
-  const avgFileDepth = d3.mean(fileDepths, d => d[1]);
-  dl.append('dt').text('Average file depth');
-  dl.append('dd').text(avgFileDepth.toFixed(2));
 }
-
-  
-
 
 const data = await loadData();
 const commits = processCommits(data);
@@ -97,6 +88,19 @@ renderCommitInfo(data, commits);
 async function renderScatterPlot() {
   const width = 1000;
   const height = 600;
+
+  // Define margins
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+  // Define usable area considering margins
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
 
   let data = await loadData();
   let commits = processCommits(data);
@@ -108,29 +112,67 @@ async function renderScatterPlot() {
     .attr('viewBox', `0 0 ${width} ${height}`)
     .style('overflow', 'visible');
 
-  // Scales
+  // Create scales
   const xScale = d3
     .scaleTime()
-    .domain(d3.extent(commits, d => d.datetime))
-    .range([0, width])
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([usableArea.left, usableArea.right])
     .nice();
 
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, 24])
-    .range([height, 0]);
+  const yScale = d3.scaleLinear().domain([0, 24]).range([usableArea.bottom, usableArea.top]);
 
-  // Draw dots
+  // Add gridlines (before the axes)
+  const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
+
+  gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+  // **Gridline coloring based on time of day**
+  gridlines.selectAll('line')
+    .attr('stroke', (d) => {
+      const hour = d; // The y value (hour) for gridline
+      // Daytime: between 6 AM and 6 PM (orange to yellow)
+      if (hour >= 6 && hour < 18) {
+        // Ensure the correct daytime color scale is applied
+        return d3.interpolateOranges((hour - 6) / 12); // Scale between 6am-6pm
+      } else {
+        // Nighttime: between 6 PM and 6 AM (blue to purple)
+        return d3.interpolateBlues((hour - 18) / 12); // Scale between 6pm-6am
+      }
+    })
+    .attr('stroke-width', 1.5); // Optionally, adjust stroke width for better visibility
+
+  // Create and add the scatter plot dots
   const dots = svg.append('g').attr('class', 'dots');
 
   dots
     .selectAll('circle')
     .data(commits)
     .join('circle')
-    .attr('cx', d => xScale(d.datetime))
-    .attr('cy', d => yScale(d.hourFrac))
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
     .attr('r', 5)
     .attr('fill', 'steelblue');
+
+  // Create the axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00'); // Formatting Y axis as time
+
+  // Add X axis
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  // Add Y axis
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
 }
 
 renderScatterPlot();
